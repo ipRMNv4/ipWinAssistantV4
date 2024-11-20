140340b58042c418bcbf49c1606e4132a12d0c41
Raman Mann <94175362+ipRMNv4@users.noreply.github.com>
2024-11-20 17:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ import psutil
 import subprocess
 import notes
 from download import download_file
-from spotify_to_yt import final
+from spotify_to_yt import final, access_token
+import sys
+
+
+def resource_path(relative_path):
+    """ Get the absolute path to the resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+# Use resource_path to load files
+with open(resource_path('access_token.txt'), 'r') as f:
+    access_token = f.read()
+
 
 def show_news(newsapi, country='in'):
     url = 'https://newsapi.org/v2/top-headlines'
@@ -55,17 +67,22 @@ def is_spotify_running():
     return False
 result1 = is_spotify_running()
 
+
+
 def final_check():
     if result1 == True and result1 == True:
         return True
 final_result = final_check()
 
-with open('access_token.txt', 'r') as f:
-    token_file = (f.read())
-token = token_file
+def resource_path(relative_path):
+    """ Get the absolute path to the resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
 
 user_headers = {
-    "Authorization": "Bearer " + token,
+    "Authorization": "Bearer " + access_token,
     "Content-Type": "application/json"
 }
 def playback_state():
@@ -104,7 +121,25 @@ def resume_playback():
             print(f"Error resuming playback: {response.status_code}")
     else:
         print("No playback state available")
-def play_next(device_id):
+
+def get_device_id():
+    url = "https://api.spotify.com/v1/me/player/devices"
+    response = requests.get(url, headers=user_headers)
+
+    if response.status_code == 200:
+        devices = response.json().get("devices", [])
+        if devices:
+            device_id = devices[0]['id']
+            return device_id
+        else:
+            print("No devices found.")
+            return None
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def play_next():
+    device_id = get_device_id()
     if final_result == True:
         url = "https://api.spotify.com/v1/me/player/next"
         response = requests.post(url, headers=user_headers, json={"device_id": device_id})
@@ -114,7 +149,8 @@ def play_next(device_id):
             print(f"Error next playback: {response.status_code}")
     else:
         print("No playback state available")
-def play_previous(device_id):
+def play_previous():
+    device_id = get_device_id()
     if final_result == True:
         url = "https://api.spotify.com/v1/me/player/previous"
         response = requests.post(url, headers=user_headers, json={"device_id": device_id})
@@ -124,12 +160,37 @@ def play_previous(device_id):
             print(f"Error previous playback: {response.status_code}")
     else:
         print("No playback state available")
+
+def currently_playing():
+    url = "https://api.spotify.com/v1/me/player/currently-playing"
+    response = requests.get(url, headers=user_headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+
+        song_name = data.get('item', {}).get('name')
+        artist_name = data.get('item', {}).get('artists', [{}])[0].get('name')
+        if song_name and artist_name:
+            return f"{song_name} by {artist_name}"
+        else:
+            return "error", "error"
+    else:
+        print("No song playing")
+        return "error", "error"
+
+
 def clean_text(text):
     cleaned_text = re.sub(r'[*]', '', text)
     return cleaned_text.strip()
 def main():
     newsapi = os.getenv('NEWS_API_KEY')
 
+    '''
+     open_spotify()
+     pause_playback()
+     resume_playback()
+    '''
     model = genai.GenerativeModel('gemini-pro')
     chat = model.start_chat(history=[])
 
@@ -173,12 +234,10 @@ def main():
                     break
 
         elif i == "open discord" or i == "open dc":
-            subprocess.Popen(r"PATH TO DISCORD.EXE")
+            subprocess.Popen(r"C:\Users\Raman Maan\AppData\Local\Discord\app-1.0.9155\Discord.exe")
 
-        elif i == "download":
-            print("Paste the download link of the file here: ")
-            linkk = input()
-            download_file(linkk)
+        elif i.startswith("https://"):
+            download_file(i)
         else:
             chat.send_message(i)
             latest_message = chat.history[-1]
